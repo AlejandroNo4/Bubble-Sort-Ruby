83330193ce0ce4a38f84730d3ac3e439ddebc923
unknown <victor.hugo.pacheco.flores@gmail.com>
2020-12-01 10:50:10 -0600

    adding bubble_sort_by function

--- index.rb
+++ index.rb
@@ -11,9 +11,30 @@ def bubble_sort (arr)
         run -= 1
     end   
     return arr
-end              
+end
+
+def bubble_sort_by (arr)
+    run=arr.length-2
+    for j in 0..arr.length-2
+        for i in 0..run
+            if (arr[i].length > arr[i+1].length)
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+            end
+        end
+        run -= 1
+    end
+    return arr
+end
 
 #test_arr = [2, 5, 8, 1, 3, 9, 10, 4, 7, 6]
+
+test_block = ["hi","hello","hey"]
 test_arr = [4,3,78,2,0,2]
+
 complete=bubble_sort(test_arr)
+block=bubble_sort_by(test_block)
+
 print complete
+print block
