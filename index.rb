3bc4551bb1596e9c6817126a5563e53cad246814
unknown <victor.hugo.pacheco.flores@gmail.com>
2020-12-01 11:04:48 -0600

    fixing linters errors 1.0

--- index.rb
+++ index.rb
@@ -1,38 +1,38 @@
-def bubble_sort (arr)
-    run=arr.length-2
-    for j in 0..arr.length - 2
-        for i in 0..run
-            if (arr[i] > arr[i+1])
-                temp = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i + 1] = temp
-            end
-        end
-        run -= 1
-    end   
-    return arr
-end
-
-def bubble_sort_by (arr)
-    run=arr.length-2
-    for j in 0..arr.length-2
-        for i in 0..run
-            if (arr[i].length > arr[i+1].length)
-                temp = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i + 1] = temp
-            end
-        end
-        run -= 1
+def bubble_sort(arr)
+  run = arr.length - 2
+  for j in 0..arr.length - 2
+    for i in 0..run
+      if arr[i] > arr[i + 1]
+        temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+      end
     end
-    return arr
+    run -= 1
+  end
+  arr
 end
 
-test_block = ["hi","hello","hey"]
-test_arr = [4,3,78,2,0,2]
+def bubble_sort_by(arr)
+  run = arr.length - 2
+  for j in 0..arr.length-2
+    for i in 0..run
+      if arr[i].length > arr[i + 1].length
+        temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+      end
+    end
+    run -= 1
+  end
+  arr
+end
 
-complete=bubble_sort(test_arr)
-block=bubble_sort_by(test_block)
+test_block = ['hi', 'hello', 'hey']
+test_arr = [4, 3, 78, 2, 0, 2]
+
+complete = bubble_sort(test_arr)
+block = bubble_sort_by(test_block)
 
 print complete
 print block
