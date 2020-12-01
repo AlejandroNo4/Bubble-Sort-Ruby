8a2f710dc719d573372003f3af1cda53dfaa4ecd
unknown <victor.hugo.pacheco.flores@gmail.com>
2020-12-01 15:23:09 -0600

    Fix bubble_sort_by function

--- index.rb
+++ index.rb
@@ -15,27 +15,29 @@ def bubble_sort(arr)
   arr
 end
 
+test_arr = [4, 3, 78, 2, 0, 2]
+n = bubble_sort(test_arr)
+puts "#{n}"
+
 def bubble_sort_by(arr)
   run = arr.length - 2
   arr.each do
     arr.each_with_index do |_, index|
       next if arr[index + 1].nil? == true
 
-      next if arr[index].length < arr[index + 1].length
-
-      temp = arr[index]
-      arr[index] = arr[index + 1]
-      arr[index + 1] = temp
+      if yield(arr[index],arr[index + 1]) > 0
+        temp = arr[index]
+        arr[index] = arr[index + 1]
+        arr[index + 1] = temp
+      end
     end
     run -= 1
   end
-  yield
+  arr
 end
 
 test_block = %w[hi hello hey]
-test_arr = [4, 3, 78, 2, 0, 2]
-
-complete = bubble_sort(test_arr)
-print complete
-
-bubble_sort_by(test_block) { puts test_block }
+a = bubble_sort_by(test_block) do |left,right|
+  left.length - right.length
+end
+puts "#{a}"
